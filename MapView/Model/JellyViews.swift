c355c84c8f7adc0313c293d18dcff93a8dab9790
Elina Lua Ming <ardentlycurious101@gmail.com>
2019-10-03 02:17:39 -0700

    Detailed label, custom callout accessories.

--- MapView/Model/JellyViews.swift
+++ MapView/Model/JellyViews.swift
@@ -28,13 +28,22 @@ class JellyView: MKAnnotationView {
             guard let jelly = newValue as? Jelly else {return}
             canShowCallout = true
             calloutOffset = CGPoint(x: -5, y: 5)
-            rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-            
+            let mapsButton = UIButton(frame: CGRect(origin: CGPoint.zero,size: CGSize(width: 30, height: 30)))
+            mapsButton.setBackgroundImage(UIImage(named: "Maps-Icon"), for: UIControl.State())
+            rightCalloutAccessoryView = mapsButton
+
             if let imageName = jelly.emojiImage {
                 image = imageName
             } else {
                 image = nil
             }
+            
+            let detailLabel = UILabel()
+            detailLabel.numberOfLines = 0
+            detailLabel.font = detailLabel.font.withSize(12)
+            detailLabel.text = jelly.eventDescription
+            detailCalloutAccessoryView = detailLabel
+
         }
     }
 }
