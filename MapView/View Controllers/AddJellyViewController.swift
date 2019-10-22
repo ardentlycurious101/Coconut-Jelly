da4e60ab847cffab738fc67cb71989535300e651
Elina Lua Ming <ardentlycurious101@gmail.com>
2019-10-22 00:02:50 -0700

    Store Jelly information including start time and end time. Create alerts to inform user when jelly information is missing, data can't be saved, or data is successfully saved.

--- MapView/View Controllers/AddJellyViewController.swift
+++ MapView/View Controllers/AddJellyViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import Firebase
 
 class AddJellyViewController: UIViewController {
         
@@ -23,18 +24,97 @@ class AddJellyViewController: UIViewController {
     
     @IBOutlet weak var MapView: MKMapView!
     
+    @IBOutlet weak var createJellyButton: UIButton!
     @IBOutlet weak var JellyCreatorDisplayName: UITextField!
+    
     
     @IBAction func CreateJellyTapped(_ sender: Any) {
         
-        // create a new Jelly instance with details
+        // check if all fields are filled
+        guard let emoji = JellyEmoji.text, emoji.count == 1 else {
+            remindUserToFill("emoji")
+            return
+        }
+        guard let name = JellyName.text, !name.isEmpty else {
+            remindUserToFill("name")
+            return
+        }
+        guard let description = JellyDescription.text, !description.isEmpty else {
+            remindUserToFill("description")
+            return
+        }
+        // TODO: Create guard statement for tags
+        // TODO: Create guard statement for start time
+//        guard let startTime =
+        // TODO: Create guard statement for end time
+        // TODO: Create guard statement for address
+        // TODO: Create guard statement for coordinates from mapview
         
-        // insert new entry in cloud database
+        guard let creatorDisplayName = JellyCreatorDisplayName.text, !creatorDisplayName.isEmpty else {
+            remindUserToFill("display name")
+            return
+        }
+        
+        // reference Firestore
+        // upon successfull networking call: insert new entry in cloud database
+        // else: print error
+
+        let _ = Firestore.firestore().collection("Jellies").addDocument(data: ["emoji" : JellyEmoji.text!, "name" : JellyName.text!, "startTime" : StartTime.date, "endTime" : EndTime.date]) { (error) in
+            
+            if let error = error {
+                
+                // debug
+                print("there's an error when saving data: \(error.localizedDescription).")
+                
+                // inform user data saving error
+                let alert = UIAlertController.init(title: "Error", message: "An error occurred when trying to save Jelly due to \(error.localizedDescription)", preferredStyle: .alert)
+                let okay = UIAlertAction.init(title: "OK", style: .default)
+                alert.addAction(okay)
+                
+                self.present(alert, animated: true)
+                
+            } else {
+                print("successfully saved data to cloud database.")
+                
+                // present alert to inform user that jelly was successfully created and saved to database
+                let alert = UIAlertController.init(title: "Success!", message: "Successfully created and saved a new Jelly.", preferredStyle: .alert)
+                let okay = UIAlertAction.init(title: "OK", style: .default)
+                alert.addAction(okay)
+                self.present(alert, animated: true)
+                
+                // clear all text fields
+                self.clearAllTextFields()
+            }
+        }
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    func remindUserToFill(_ textFieldRequiredInput: String) {
+        let okay = UIAlertAction.init(title: "OK", style: .default)
+        let alert = UIAlertController.init(title: "Missing Jelly Information", message: "Please enter Jelly \(textFieldRequiredInput).", preferredStyle: .alert)
+        alert.addAction(okay)
+        self.present(alert, animated: true) {}
+    }
+    
+    func configureUI() {
+        createJellyButton.layer.cornerRadius = 25
+    }
+    
+    func clearAllTextFields() {
+        JellyEmoji.text = ""
+        JellyName.text = ""
+        JellyDescription.text = ""
+        JellyTags.text = ""
+        
+        StartTime.date = Date()
+        EndTime.date = Date()
+        
+        JellyCreatorDisplayName.text = ""
     }
 
 }
